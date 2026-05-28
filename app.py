c03d82bcbba1d3378dc531ea6228935f32711181
Daria <dashikozh@gmail.com>
2026-05-28 11:52:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,9 +53,5 @@ def index():
     
     return render_template('index.html', result=result)
 
-@app.route('/hello')
-def hello_world():
-    return 'Привет, мир!'
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
